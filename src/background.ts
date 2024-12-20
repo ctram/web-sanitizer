d189bc98b8985bfb0c52380c96485ee36e3dfb6b
Chris Tram <ctram@proton.me>
2024-12-20 11:59:52 -0800

    Convert to typescript project

---
+++ src/background.ts
@@ -0,0 +1,8 @@
+chrome.runtime.onInstalled.addListener(function() {
+    console.log('Extension installed');
+  });
+  
+  chrome.action.onClicked.addListener(function(tab) {
+    console.log('Extension icon clicked');
+  });
+  
---
+++ src/popup.ts
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var button = document.getElementById("myButton");
+  button!.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const tabId = tabs[0].id ?? 0;
+      chrome.tabs.sendMessage(tabId, { action: "greet" });
+    });
+  });
+});
